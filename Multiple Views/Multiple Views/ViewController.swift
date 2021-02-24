2d1624d4259c527818424f674c4c51b17eea5591
Richard <>
2021-02-24 10:50:34 -0500

    got a ball that moves across the screen

--- Multiple Views/Multiple Views/ViewController.swift
+++ Multiple Views/Multiple Views/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var ball : UIButton!
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func startBallMoving(){
+        Timer.scheduledTimer(timeInterval : 0.05, target: self, selector: #selector(ViewController.moveBall), userInfo: nil, repeats: true)
+    }
+    
+    @objc func moveBall(){
+        ball.center.x += 13.0
+        ball.center.y += 13.0
+    }
 
 }
 

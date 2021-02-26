b5816a45243a1c1d47256448b2e8f83bcc8935fa
Richard <>
2021-02-26 14:32:16 -0500

    added multiple screens, the ball goes from the top screen to the bottom screen

--- Multiple Views/Multiple Views/SecondViewController.swift
+++ Multiple Views/Multiple Views/SecondViewController.swift
@@ -8,22 +8,62 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet var ball : UIButton!
+    
+//    let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipe))
+    var FirstViewController : ViewController?
+    
+    var xPosition:CGFloat = 0
+    var timer = Timer()
+    var horizontalMovement:CGFloat = 13.0
+    var verticalMovement:CGFloat = 13.0
+    
+    var leftEdge:CGFloat = 0.0
+    var rightEdge:CGFloat = UIScreen.main.bounds.size.width
+    var topEdge:CGFloat = 0.0
+    var bottomEdge:CGFloat = UIScreen.main.bounds.size.height
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        ball.center.y = 0.0
+        ball.center.x = xPosition
+        startBallMoving()
+//        FirstViewController?.swipeUp.direction = UISwipeGestureRecognizer.Direction.down
+//        print(FirstViewController!.swipeUp)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if (segue.destination is ViewController){
+            let firstVC = segue.destination as? ViewController
+            firstVC?.xPosition = ball.center.x
+            firstVC?.horizontalMovement = 13.0
+            firstVC?.verticalMovement = -13.0
+        }
     }
-    */
 
+    @IBAction func startBallMoving(){
+        ball.isHidden = false
+        Timer.scheduledTimer(timeInterval : 0.05, target: self, selector: #selector(SecondViewController.moveBall), userInfo: nil, repeats: true)
+    }
+    
+    @objc func moveBall(){
+        ball.center.x += horizontalMovement
+        ball.center.y += verticalMovement
+        if(ball.center.x >= rightEdge || ball.center.x <= leftEdge ){
+            horizontalMovement = -horizontalMovement
+        }
+        if(ball.center.y <= topEdge){
+            verticalMovement = 0
+            horizontalMovement = 0
+            ball.isHidden = true
+        }
+        if(ball.center.y >= bottomEdge){
+            verticalMovement = -verticalMovement
+        }
+    }
 }
--- Multiple Views/Multiple Views/ViewController.swift
+++ Multiple Views/Multiple Views/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var ball : UIButton!
+//    @IBOutlet weak var myView : UIView!
+//    let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(SwipeUp(sender:)))
+    //var swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(SwipeDown))
+
     var timer = Timer()
+    var horizontalMovement:CGFloat = 13.0
+    var verticalMovement:CGFloat = 13.0
+    
+    var leftEdge:CGFloat = 0.0
+    var rightEdge:CGFloat = UIScreen.main.bounds.size.width
+    var topEdge:CGFloat = 0.0
+    var bottomEdge:CGFloat = UIScreen.main.bounds.size.height
+    
+    var xPosition:CGFloat = 0.0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(SwipeUp))
+//        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+//        myView.addGestureRecognizer(swipeUp)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("ksjgnakjg")
+        if (segue.destination is SecondViewController){
+            let secondVC = segue.destination as? SecondViewController
+            secondVC?.xPosition = ball.center.x
+            secondVC?.horizontalMovement = 13.0
+            secondVC?.verticalMovement = 13.0
+            secondVC?.FirstViewController = self
+        }
     }
 
     @IBAction func startBallMoving(){
@@ -22,9 +50,48 @@ class ViewController: UIViewController {
     }
     
     @objc func moveBall(){
-        ball.center.x += 13.0
-        ball.center.y += 13.0
+        ball.center.x += horizontalMovement
+        ball.center.y += verticalMovement
+        if(ball.center.x >= rightEdge || ball.center.x <= leftEdge ){
+            horizontalMovement = -horizontalMovement
+        }
+        if(ball.center.y <= topEdge){
+            verticalMovement = -verticalMovement
+        }
+        if(ball.center.y >= bottomEdge){
+            verticalMovement = 0
+            horizontalMovement = 0
+            ball.isHidden = true
+        }
+        
+        
+//        if (navigationController?.viewControllers) != nil {
+//            if(navigationController?.viewControllers.count == 1){
+//                startAgain()
+//            }
+//        }
     }
-
+//    @objc func SwipeUp(gesture: UIGestureRecognizer){
+//        print("Here")
+//        print(gesture)
+//        self.performSegue(withIdentifier: "GoToSecondViewSegue", sender: nil)
+//    }
+//    @objc func SwipeUp(sender: UITapGestureRecognizer){
+//        print("Here")
+////        print(gesture)
+//        self.performSegue(withIdentifier: "GoToSecondViewSegue", sender: nil)
+//    }
+//    @objc func SwipeDown(){
+//        startAgain()
+//    }
+//
+//    func startAgain(){
+//        //swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+//        ball.center.x = xPosition
+//        ball.center.y = bottomEdge
+//        horizontalMovement = 13.0
+//        verticalMovement = 13.0
+//        ball.isHidden = false
+//    }
 }
 
